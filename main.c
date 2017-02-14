39e47b0b30576be24e4b8d1c7ec000eda15edd05
mehrdadzahraei <mehrdad.zahraei@etu.univ-orleans.fr>
2017-02-14 10:18:19 +0100

    Ajouter 2 autres fichiers

---
+++ main.c
@@ -0,0 +1,8 @@
+#include "header.h"
+
+int main()
+{
+	saySomthing();
+	
+	return 0;
+}
---
+++ saySomthing.c
@@ -0,0 +1,8 @@
+#include "header.h"
+
+#include <stdio.h>
+
+void saySomthing()
+{
+	printf("This is just a simple example for exam\n");
+}
